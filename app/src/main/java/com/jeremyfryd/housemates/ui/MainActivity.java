43cf78723927dcfa7ed0bd6b7d2fa1dbd2340e87
Jeremy Fryd <fryd21@gmail.com>
2016-12-19 13:37:12 -0800

    complete firebase authentication setup

--- app/src/main/java/com/jeremyfryd/housemates/ui/MainActivity.java
+++ app/src/main/java/com/jeremyfryd/housemates/ui/MainActivity.java
@@ -1,15 +1,25 @@
 package com.jeremyfryd.housemates.ui;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.jeremyfryd.housemates.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+
 }
